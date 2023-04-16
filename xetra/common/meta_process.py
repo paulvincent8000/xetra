12ce3fecd98604631928324213660dd33288a390
Paul Vincent <vince8000@gmail.com>
2023-04-16 16:38:36 +0200

    initial setup

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,21 @@
+class MetaProcess():
+    '''
+    Methods for processing the meta file
+    '''
+    
+    def __init__(self) -> None:
+        '''
+        Class for working with the meta file
+        '''
+        pass
+
+    @staticmethod    
+    def update_meta_file():
+        pass
+
+    @staticmethod
+    def return_date_list():
+        pass
+
+    def __str__(self) -> str:
+        pass
