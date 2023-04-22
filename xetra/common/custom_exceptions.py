872bbd1711af68406da2e621d2d2587c970be630
Paul Vincent <vince8000@gmail.com>
2023-04-22 20:56:38 +0200

    adds functions from course

--- xetra/common/custom_exceptions.py
+++ xetra/common/custom_exceptions.py
@@ -1,11 +1,17 @@
-''' Custom Exceptions'''
+"""Custom Exceptions"""
 
 class WrongFormatException(Exception):
-    '''
-    Wrong format exception class
-    
-    Exception that can be raised when the format type
-    given as parameter is not supported
-    '''
+    """
+    WrongFormatException class
 
-    
+    Exception that can be raised when the format type
+    given as parameter is not supported.
+    """
+
+class WrongMetaFileException(Exception):
+    """
+    WrongMetaFileException class
+
+    Exception that can be raised when the meta file
+    format is not correct.
+    """
